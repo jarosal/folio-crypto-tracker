c8541e1f4058edd1fbc6a92871f58e20ddb82da4
Jarosal <azethis@gmail.com>
2022-01-23 18:40:53 +0100

    added aliases

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import { resolve } from "path";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -15,7 +16,15 @@ const config = {
 		adapter: adapter(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		vite: {
+			resolve: {
+				alias: {
+					$components: resolve('./src/components'),
+					$stores: resolve('./src/stores')
+				}
+			}
+		}
 	}
 };
 
