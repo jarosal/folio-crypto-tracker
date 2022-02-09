678b828d9f1fa84e433b732a0e515e7a9f90f698
Jarosal <azethis@gmail.com>
2022-02-09 18:36:46 +0100

    added theme switching logic and button

--- src/lib/components/nav/ThemeSwitch.svelte
+++ src/lib/components/nav/ThemeSwitch.svelte
@@ -1,19 +1,35 @@
-<script>
-	import themeStore, { setTheme } from 'svelte-themes';
+<script lant="ts">
+	import themeStore from 'svelte-themes';
+
+	function switchTheme() {
+		$themeStore.theme = $themeStore.theme === 'dark' ? 'light' : 'dark';
+	}
 </script>
 
-<!-- <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-	<path
-		stroke-linecap="round"
-		stroke-linejoin="round"
-		stroke-width="2"
-		d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-	/>
-</svg> -->
-
-<select bind:value={$themeStore.theme}>
-	<option value="dark">Dark</option>
-	<option value="light">Light</option>
-</select>
-
-<button on:click={() => setTheme('dark')}> Dark mode </button>
+<button
+	aria-label="Toggle Dark Mode"
+	class="ml-1 flex h-9 w-9 items-center justify-center rounded-lg bg-yellow-400 ring-yellow-400
+			transition-all hover:ring-2 dark:bg-yellow-800"
+	on:click={switchTheme}
+>
+	{#if $themeStore.theme === 'light'}
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" class="h-5 w-5 text-gray-800 dark:text-yellow-100">
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728
+						0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+			/>
+		</svg>
+	{:else}
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" class="w-5 h-5 text-gray-800 dark:text-gray-200">
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+			/>
+		</svg>
+	{/if}
+</button>
