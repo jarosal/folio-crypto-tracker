09df2f9bc5741d6c1db03e919b30473dc222e05a
Jarosal <azethis@gmail.com>
2022-02-13 12:47:21 +0100

    added initial user panel styling

--- src/lib/components/nav/UserPanel.svelte
+++ src/lib/components/nav/UserPanel.svelte
@@ -1,4 +1,4 @@
-<div class="flex items-center">
-	<a class="mx-4 text-lg" href="/">Login</a>
-	<a class="mx-4 text-lg " href="/">Register</a>
+<div class="hidden sm:flex sm:items-center mx-4">
+	<a href="#" class="text-white text-base font-semibold hover:text-purple-600 mr-4">Login</a>
+	<a href="#" class="text-white text-base font-semibold border px-4 py-2 rounded-lg hover:text-purple-600 hover:border-purple-600 h-10">Register</a>
 </div>
