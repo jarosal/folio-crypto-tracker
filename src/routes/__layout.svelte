0705bc09974caebed9de3ee66f282213b6bcdabc
Jarosal <azethis@gmail.com>
2022-03-03 20:07:16 +0100

    added initial logo

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -9,7 +9,7 @@
 
 <main class="h-screen flex items-center px-6 lg:px-32 bg-purple-900 text-white">
 	<Nav />
-	<section class="w-full grid place-items-center h-screen">
+	<section class="w-full grid grid-cols-1 row-start-1 place-items-center h-screen">
 		<slot />
 	</section>
 	<Footer />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,9 @@
+<script lang="ts">
+	import FolioBigLogo from '$lib/components/common/FolioBigLogo.svelte';
+</script>
+
+<div class="h-12">
+	<FolioBigLogo />
+</div>
+
 <div class="w-1/2 h-96 bg-red-300 mx-5 dark:bg-zinc-50" />
