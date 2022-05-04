c58ed0c686c51043c9143887cf2d64082d1374c6
Jarosal <azethis@gmail.com>
2022-05-04 16:50:31 +0200

    added custom colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,32 @@ const config = {
 			minHeight: {
 				'layout': 'calc(100vh - 240px)',
 			},
+			colors: {
+				'eggshell': {
+					50: '#F9F1DC'
+				},
+				'mustard': {
+					200: '#FFCB69',
+					800: '#8F5D00'
+				},
+				'lavender': {
+					700: '#854768',
+					800: '#77405E'
+				},
+				'flame': {
+					300: '#FF475A'
+				},
+				'olive': {
+					500: '#7CB06D'
+				},
+				'eerie': {
+					900: '#191919'
+				},
+				'raisin': {
+					700: '#2C262C',
+					800: '#221C22'
+				}
+			},
 		}
 	},
 
