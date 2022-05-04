5717984e29ce8c7cd558e2e93ba0619446eb7b06
Jarosal <azethis@gmail.com>
2022-05-04 18:38:59 +0200

    styled footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,14 +1,14 @@
-<footer class="text-white p-4 lg:p-10 ">
-	<p class="font-bold font-sans text-center text-sm">
+<footer class="font-karma font-normal text-eggshell-50 text-center text-base p-10">
+	<p>
 		powered by
-		<a href="https://kit.svelte.dev" rel="noopener" target="_blank" class="hover:text-pink-400">SvelteKit</a>
+		<a href="https://kit.svelte.dev" rel="noopener" target="_blank" class="hover:text-eggshell-200 font-semibold">SvelteKit</a>
 		and
-		<a href="https://www.coingecko.com/en/api" rel="noopener" target="_blank" class="hover:text-pink-400">CoinGecko API</a>
+		<a href="https://www.coingecko.com/en/api" rel="noopener" target="_blank" class="hover:text-eggshell-200  font-semibold">CoinGecko API</a>
 		· source code available on
-		<a href="https://github.com/jarosal/folio-crypto-tracker" rel="noopener" target="_blank" class="hover:text-pink-400">GitHub</a>
+		<a href="https://github.com/jarosal/folio-crypto-tracker" rel="noopener" target="_blank" class="hover:text-eggshell-200  font-semibold">GitHub</a>
 	</p>
-	<p class="text-center text-sm">
+	<p>
 		copyright © 2022,
-		<a href="https://github.com/jarosal" rel="noopener" target="_blank" class="hover:text-pink-400">Jaroslaw Sliwinski</a>
+		<a href="https://github.com/jarosal" rel="noopener" target="_blank" class="hover:text-eggshell-200 ">Jaroslaw Sliwinski</a>
 	</p>
 </footer>
