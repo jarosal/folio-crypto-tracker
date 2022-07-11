366a987d721c2895ec6555b84dc672ada76add75
Jarosal <azethis@gmail.com>
2022-07-11 20:48:05 +0200

    upgraded to latest sveltekit

--- svelte.config.js
+++ svelte.config.js
@@ -1,28 +1,19 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-import { resolve } from 'path';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [
-		preprocess({
-			postcss: true
-		})
-	],
-
+	preprocess: preprocess(),
 	kit: {
 		adapter: adapter(),
-
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		vite: {
-			resolve: {
-				alias: {
-					src: resolve('./src')
-				}
-			}
+		alias: {
+			$components: 'src/components',
+			$stores: 'src/stores',
+			$logic: 'src/logic',
+			$data: 'src/data'
 		}
 	}
 };
